58dc37701b07aee2a62265a09c79704dc977ac63
Surafel Yimam <63095824+surafelx@users.noreply.github.com>
2025-02-03 17:11:21 +0300

    Cors modiification again

--- app.js
+++ app.js
@@ -7,14 +7,9 @@ const passport = require("passport");
 //const colyseus = require("colyseus.js");
 require("dotenv").config();
 
-const corsOptions = {
-  origin: '*', // Allow all origins, or you can restrict this to specific domains like: ['https://inquisitive-sunburst-5396b5.netlify.app']
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true, // if you need cookies or authentication
-};
 
-app.use(cors(corsOptions));
+
+app.use(cors());
 
 // Handling preflight requests (OPTIONS)
 app.options('*', cors(corsOptions));
@@ -34,3 +29,5 @@ gameServer.listen(process.env.PORT);
 app.listen(process.env.PORT, () => {
   console.log("server started sur le port " + process.env.PORT);
 });
+
+
