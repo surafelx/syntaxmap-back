c5c00343fd4d50a588ac041f7225cbd38bfd9a9a
Surafel Yimam <63095824+surafelx@users.noreply.github.com>
2025-02-03 15:09:20 +0300

    Pushed for origin true

--- app.js
+++ app.js
@@ -3,26 +3,24 @@ const app = express();
 const bodyParser = require("body-parser");
 const http = require("http");
 const cors = require("cors");
-const passport = require('passport');
+const passport = require("passport");
 //const colyseus = require("colyseus.js");
-require('dotenv').config();
+require("dotenv").config();
 
-const urls = ['http://localhost:3000', 'https://linguistic-com.herokuapp.com', 'https://linguistic-com-qa.herokuapp.com']
-
-app.use(cors({allowedOrigins: urls}));
+app.use(cors({ origin: "*" }));
 
 app.use(bodyParser.json());
 
-require('./config/passportConfig')(passport);
+require("./config/passportConfig")(passport);
 app.use(passport.initialize());
 
-
-require('./index-v2.js')(app);
-
+require("./index-v2.js")(app);
 
 /*const gameServer = new colyseus.Server({
   server: http.createServer(app)
 });
 gameServer.listen(process.env.PORT);
 */
-app.listen(process.env.PORT, () => {console.log("server started sur le port " + process.env.PORT)});
+app.listen(process.env.PORT, () => {
+  console.log("server started sur le port " + process.env.PORT);
+});
