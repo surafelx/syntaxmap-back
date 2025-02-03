0839bc674a62a1c15d5a6513156f7d3abc15ed15
Surafel Yimam <63095824+surafelx@users.noreply.github.com>
2025-02-03 21:33:00 +0300

    Origin true

--- app.js
+++ app.js
@@ -6,16 +6,8 @@ require("dotenv").config();
 
 const app = express();
 
-// Apply CORS middleware first
-const corsOptions = {
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,  // Allow cookies and credentials
-};
-
-app.use(cors(corsOptions));  // CORS middleware must be at the top
-app.options('*', cors(corsOptions));  // Handle preflight OPTIONS requests
+app.use(cors({ origin: true }));
+app.options("*", cors(corsOptions)); // Handle preflight OPTIONS requests
 
 app.use(bodyParser.json());
 require("./config/passportConfig")(passport);
