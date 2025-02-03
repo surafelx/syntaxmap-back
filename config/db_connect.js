8a19dbfa55a5a67a8255848f4dc5ceb73c6298a7
Surafel Yimam <63095824+surafelx@users.noreply.github.com>
2025-02-03 14:05:08 +0300

    Added reject unauthorized

--- config/db_connect.js
+++ config/db_connect.js
@@ -1,15 +1,16 @@
-const { Pool, Client } = require('pg')
-require('dotenv').config();
+const { Pool, Client } = require("pg");
+require("dotenv").config();
 
-const connectionString = process.env.DATABASE_URL
-
+const connectionString = process.env.DATABASE_URL;
 
 const pool = new Pool({
   user: process.env.DATABASE_USER,
-  host:  process.env.DATABASE_HOST,
+  host: process.env.DATABASE_HOST,
   database: process.env.DATABASE_NAME,
   password: process.env.DATABASE_PASSWORD,
   port: process.env.DATABASE_PORT,
-  ssl: true, // Disable SSL
+  ssl: {
+    rejectUnauthorized: false,
+  }, 
 });
 module.exports = pool;
