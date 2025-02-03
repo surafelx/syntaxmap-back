265ce6e545fbd6f06c3839bb457f7bb3951e7a98
Surafel Yimam <63095824+surafelx@users.noreply.github.com>
2025-02-03 21:20:48 +0300

    Origins allowed

--- app.js
+++ app.js
@@ -1,40 +1,28 @@
 const express = require("express");
-const app = express();
-const bodyParser = require("body-parser");
-const http = require("http");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const passport = require("passport");
-//const colyseus = require("colyseus.js");
 require("dotenv").config();
 
+const app = express();
 
+// Apply CORS middleware first
 const corsOptions = {
-  origin: 'https://syntaxmap-front-2z2b.onrender.com',  // Frontend domain
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,  // Allow cookies and credentials
 };
 
-app.use(cors(corsOptions));  // Apply CORS middleware
+app.use(cors(corsOptions));  // CORS middleware must be at the top
 app.options('*', cors(corsOptions));  // Handle preflight OPTIONS requests
 
-
-// Handling preflight requests (OPTIONS)
-
 app.use(bodyParser.json());
-
 require("./config/passportConfig")(passport);
 app.use(passport.initialize());
 
 require("./index-v2.js")(app);
 
-/*const gameServer = new colyseus.Server({
-  server: http.createServer(app)
-});
-gameServer.listen(process.env.PORT);
-*/
 app.listen(process.env.PORT, () => {
   console.log("server started sur le port " + process.env.PORT);
 });
-
-
