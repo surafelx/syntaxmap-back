8719349e6c831ccab16859fbd63946319c1ee27b
Surafel Yimam <63095824+surafelx@users.noreply.github.com>
2025-02-03 13:55:55 +0300

    Updated Port

--- config/db_connect.js
+++ config/db_connect.js
@@ -9,7 +9,7 @@ const pool = new Pool({
   host:  process.env.DATABASE_HOST,
   database: process.env.DATABASE_NAME,
   password: process.env.DATABASE_PASSWORD,
-  port: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT,
   ssl: true, // Disable SSL
 });
 module.exports = pool;
