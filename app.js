087408e75049d9b33fd05e02cb132cf53f188d64
Surafel Yimam <63095824+surafelx@users.noreply.github.com>
2025-02-05 15:24:47 +0300

    Trying to fix cors [STILL]

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
-
-
+// app.use(cors());
 app.use(bodyParser.json());
+
 require("./config/passportConfig")(passport);
 app.use(passport.initialize());
 
