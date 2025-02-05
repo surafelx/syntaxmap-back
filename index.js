97a771ccdbfb597968a93b8a6a137a70671d854d
Surafel Yimam <63095824+surafelx@users.noreply.github.com>
2025-02-05 15:51:26 +0300

    Added CORS Policy back

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 require("dotenv").config();
 
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 app.use(bodyParser.json());
 
 require("./config/passportConfig")(passport);
